426954257c9d1163017f6940e2827f7562465b5a
JossBie <163793515+JossBie@users.noreply.github.com>
2024-07-01 17:09:13 +0800

    Create RestartService.sh

---
+++ Scripts/RestartService.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Hello, we wil restart service now-----
+#sh /home/bielei/apache-tomcat-9.0.90/bin/shutdown.sh
+sleep 30
+#sh /home/bielei/apache-tomcat-9.0.90/bin/startup.sh.sh
+echo Restart service finished-----
