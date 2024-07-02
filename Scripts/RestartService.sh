59b3bc36d3d8a5afc6d001bff58168ac014f0a4b
JossBie <163793515+JossBie@users.noreply.github.com>
2024-07-02 09:49:56 +0800

    Update RestartService.sh

--- Scripts/RestartService.sh
+++ Scripts/RestartService.sh
@@ -2,5 +2,5 @@
 echo Hello, we wil restart service now-----
 sh /home/bielei/apache-tomcat-9.0.90/bin/shutdown.sh
 sleep 30
-sh /home/bielei/apache-tomcat-9.0.90/bin/startup.sh.sh
+sh /home/bielei/apache-tomcat-9.0.90/bin/startup.sh
 echo Restart service finished-----
